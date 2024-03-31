36c8d90129e35bccdc37bf772d4433f8baa893b1
Lucid <100102668+dearmysolitude@users.noreply.github.com>
2024-03-31 11:25:09 +0900

    [laddergame] upload

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # 자바
 소규모 자바 코드들을 모아 놓은 레포지토리입니다. 규모가 커지면 향후 분리할 수 있음.
 
-## 기초 구현
+## 구현
 
 - CalendarApp.java: 만년 달력
 - Practice.java: 10 진수를 3 진수로 바꾸는 프로그램
-- Pass by value.java: 자바는 **값 복사**로 매개 변수를 전달한다.
 
 ## 알고리즘
+/algorithm/
 
-- cleaningRobot: 청소 로봇
-- ladderGame: 사다리 게임
+- leaningrobot: 청소 로봇
+- laddergame: 사다리 게임
 
 ## 미니 프로그램
 
 - chattingapp: 채팅 앱
 - membermanageapp: 사용자 관리 앱
+
+## 테스트
+
+- Pass by value.java: 자바는 **값 복사**로 매개 변수를 전달한다.
--- src/main/java/algorithm/laddergame/Solution.java
+++ src/main/java/algorithm/laddergame/Solution.java
@@ -1,5 +1,41 @@
 package algorithm.laddergame;
 
-public class Solution {
-    
+import java.util.*;
+class Solution {
+    public char[] solution(int n, int[][] ladder){
+        char[] answer = new char[n];
+        
+        // 주의: ladder의 사다리는 1 부터 인덱스가 시작됨!
+        for(int i = 0; i < n; i++) { // A 부터 n개의 대문자를
+            answer[afterLadder(i, ladder)] = (char)(i + 65); // answer의 새로운 인덱스에 넣어준다.
+        }
+        
+        return answer;
+    }
+
+    // 사다리 게임 후 변화된 인덱스를 반환. 
+    public static int afterLadder(int index, int[][] ladder) { 
+        for(int i = 0; i < ladder.length; i ++) { // 사다리 칸을 내려가면서
+            for(int ladderNumber : ladder[i]) { // 그 열의 사다리를 왼쪽부터 오른쪽으로 훑는다.
+                if (index + 1 == ladderNumber) { // 오른쪽으로 이동하는 경우
+                    index ++;
+                    continue;
+                }
+                if(index + 1 == ladderNumber + 1) { // 왼쪽으로 이동하는 경우
+                    index --;
+                }
+            }
+//            System.out.println((char)(index+65)+"의 "+ i + " 줄 통과 후 위치: "+ index);
+        }
+        return index;
+    }
+
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
+        System.out.println(Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+        System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+        System.out.println(Arrays.toString(T.solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
+    }
 }
+
